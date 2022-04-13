1c6f908dc3a6d54f3efd62ef72cacbd8a8aa47c4
Zeynep Aşgel <zeynep@Zeyneps-MacBook-Air.local>
2022-04-13 22:18:30 +0200

    final modification

--- Percentage.py
+++ Percentage.py
@@ -11,23 +11,18 @@ if len(sys.argv) == 1:
 args = parser.parse_args()
 args.seq = args.seq.upper()
 if re.search('^[ACGTU]+$', args.seq):
-
     u= args.seq.count("U") #this counts the U Percentage
     a= args.seq.count("A") #this counts the A Percentage
     c= args.seq.count("C") #this counts the C Percentage
     t= args.seq.count("T") #this counts the T Percentage
     g= args.seq.count("G") #this counts the G Percentage
-    percent_U=float(float(U*100)/float(len(args.seq)))
-    percent_A=float(float(A*100)/float(len(args.seq)))
-    percent_C=float(float(C*100)/float(len(args.seq)))
-    percent_T=float(float(T*100)/float(len(args.seq)))
-    percent_G=float(float(G*100)/float(len(args.seq)))
+
     print ("% of U in sequence is:", (u / len(args.seq)) * 100)#Calulate and Print the U Percentage
     print ("% of A in sequence is:", (a / len(args.seq)) * 100)#Calculate and Print the A Percentage
     print ("% of C in sequence is:", (c / len(args.seq)) * 100)#Calculate and Print the C Percentage
-    print ("% of T in sequence:", percent_T, "%")#Print the T Percentage
-    print ("% of G in sequence:", percent_G, "%")#Print the G Percentage
+    print ("% of T in sequence is:", (t / len(args.seq)) * 100)#Calculate and Print the T Percentage
+    print ("% of G in sequence is:", (g / len(args.seq)) * 100)#Calculate and Print the G Percentage
+
 else:
     print ('The sequence is not DNA or RNA')
-    
     
